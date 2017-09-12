1807807a783140e3307cbdda2be79de42450e452
ljhcage <120989324@qq.com>
2017-09-12 19:58:17 +0800

    Update block.bat
    增加替换边下边播播放器为potplay的功能

--- block.bat
+++ block.bat
@@ -3,3 +3,6 @@ D:
 cd D:\ProgramsFiles\Thunder\Data\ThunderPush\
 cd.>thunder7forceupgrade.xar
 attrib +s thunder7forceupgrade.xar
+
+echo [global]>D:\ProgramsFiles\Thunder\Program\xmp.ini
+echo path="E:\Program Files (x86)\DAUM\PotPlayer\PotPlayerMini.exe">>D:\ProgramsFiles\Thunder\Program\xmp.ini
