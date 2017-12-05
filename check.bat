f7b8e5949e42a107484211f9a17e817f94e9293b
lcjh <120989324@qq.com>
2017-12-06 00:29:09 +0800

    add check.bat
    add check.bat to hash file with MD5, SHA-1, SHA-256 and SHA-512. 增加使用MD5，SHA-1，SHA256，SHA512校验文件的脚本，注释里面包含使用python的方法，但是python会把文件一次性读取到内存，对大文件不友好

---
+++ check.bat
@@ -0,0 +1,18 @@
+@echo off
+:start
+echo 请输入文件名
+set /p filename=
+if /i %filename% EQU exit exit
+@rem echo MD5
+@rem python -c "import hashlib,sys;print hashlib.md5(open(sys.argv[1],'rb').read()).hexdigest()" %filename%
+@rem echo SHA-1
+@rem python -c "import hashlib,sys;print hashlib.sha1(open(sys.argv[1],'rb').read()).hexdigest()" %filename%
+@rem echo SHA-256
+@rem python -c "import hashlib,sys;print hashlib.sha256(open(sys.argv[1],'rb').read()).hexdigest()" %filename%
+@rem echo SHA-512
+@rem python -c "import hashlib,sys;print hashlib.sha512(open(sys.argv[1],'rb').read()).hexdigest()"  %filename%
+powershell "Get-FileHash %filename% -Algorithm MD5 | Format-List"
+powershell "Get-FileHash %filename% -Algorithm SHA1 | Format-List"
+powershell "Get-FileHash %filename% -Algorithm SHA256 | Format-List"
+powershell "Get-FileHash %filename% -Algorithm SHA512 | Format-List"
+goto start
